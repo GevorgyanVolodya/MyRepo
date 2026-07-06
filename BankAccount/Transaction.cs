9b25dbf34e81411689ab6f56b9cdb688cd4c4b87
GevorgyanVolodya <v8olodya@gmail.com>
2026-07-06 23:52:20 +0400

    Add files via upload

---
+++ BankAccount/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+abstract class Transaction
+{
+    public Guid Id { get; }
+    public decimal Amount { get; }
+    public DateTime Date { get; }
+    public bool IsCompleted { get; protected set; }
+
+    protected Transaction(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Amount must be positive");
+        }
+
+        Id = Guid.NewGuid();
+        Amount = amount;
+        Date = DateTime.Now;
+        IsCompleted = false;
+    }
+
+    public abstract void Execute();
+}
